3d74e7f3d0dce4b26b088858f8f3d3336e95ce80
Ghaziah Shoeb <116344438+GhaziahShoeb@users.noreply.github.com>
2026-04-09 23:25:05 +0530

    Time: 9 ms (12.92%) | Memory: 174.6 MB (82.55%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.java
@@ -1,30 +1,18 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        List <Integer> pos = new ArrayList<>();
-        List <Integer> neg = new ArrayList<>();
+        List<Integer> pos = new ArrayList<>();
+        List<Integer> neg = new ArrayList<>();
 
-        for(int i =0 ; i<nums.length;i++){
-            if(0<nums[i]){
-                pos.add(nums[i]);
-            }else{
-                neg.add(nums[i]);
-            }
+        for (int num : nums) {
+            if (num > 0) pos.add(num);
+            else neg.add(num);
         }
-        for(int i =0;i<nums.length/2;i++){
-            nums[2*i] = pos.get(i);
-            nums[2*i+1] = neg.get(i);
+
+        for (int i = 0; i < nums.length / 2; i++) {
+            nums[2 * i] = pos.get(i);
+            nums[2 * i + 1] = neg.get(i);
         }
+
         return nums;
-    }
-    public static void main(String[] args){
-        int[] A = {1,2,-4,-5};
-        int n = A.length;
-
-        Solution obj = new Solution();
-        int[] result = obj.rearrangeArray(A);
-
-        for(int num:result){
-            System.out.print(num+" ");
-        }
     }
 }
