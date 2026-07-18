8649ddd088a4459f7670d3799d35b1046689eaea
Ghaziah Shoeb <116344438+GhaziahShoeb@users.noreply.github.com>
2026-07-18 16:14:42 +0530

    Time: 5 ms (61.42%) | Memory: 44.5 MB (67.03%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -2,17 +2,22 @@ class Solution {
     public boolean isAnagram(String str1, String str2) {
         if(str1.length() != str2.length()){
             return false;
-        }
-        char[] charArray1 = str1.toCharArray();
-        char[] charArray2 = str2.toCharArray();
-        java.util.Arrays.sort(charArray1);
-        java.util.Arrays.sort(charArray2);
 
-        for(int i = 0 ; i< str1.length();i++){
-            if(charArray1[i] != charArray2[i]){
+        }   
+        int[] freq = new int[26];
+
+        for(int i = 0 ; i< str1.length(); i++){
+            freq[Character.toUpperCase(str1.charAt(i)) - 'A' ]++; // Increment frequency for each character in str1
+        }
+        for(int i = 0 ; i < str2.length(); i++){
+            freq[Character.toUpperCase(str2.charAt(i)) - 'A' ]--; // Decrement frequency for each character in str2
+
+        }   
+        for(int i = 0 ; i< 26; i++){
+            if(freq[i] != 0){
                 return false;
             }
         }
-        return true;
+        return true;  
     }
 }
