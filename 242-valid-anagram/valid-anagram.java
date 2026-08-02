b4f49a7d5e6838734621021d67cf3f40048705f5
Ghaziah Shoeb <116344438+GhaziahShoeb@users.noreply.github.com>
2026-08-03 02:05:31 +0530

    Time: 6 ms (43.08%) | Memory: 44.5 MB (60.59%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,12 +3,21 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
+        int[] freq = new int[26];
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        return Arrays.equals(arr1,arr2);
+        for(int i = 0; i< s.length();i++){
+            freq[Character.toUpperCase(s.charAt(i)) - 'A']++;
+            
+            
+        }
+        for(int j = 0 ; j<t.length();j++){
+            freq[Character.toUpperCase(t.charAt(j))-'A']--;
+        }
+        for(int i= 0; i<26;i++){
+            if(freq[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
