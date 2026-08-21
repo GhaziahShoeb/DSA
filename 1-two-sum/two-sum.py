d21808004089e0f47785f6b0d4d7210e2a822076
Ghaziah Shoeb <116344438+GhaziahShoeb@users.noreply.github.com>
2026-08-21 13:13:00 +0530

    Time: 11 ms (35.85%) | Memory: 20.8 MB (6.90%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,10 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = {}
-        for index , value in enumerate(nums):
-         complement = target - value
-         if complement in seen:
-          return [seen[complement],index]
-         seen[value] = index
-        return []
+        paired = []
+        for idx in range(len(nums)):
+            value = nums[idx]
+            paired.append([value,idx])
+        paired.sort()
 
+        left = 0
+        right = len(paired) -1 
+
+        while left < right :
+            left_value = paired[left][0]
+            right_value = paired[right][0]
+            curr_sum = left_value + right_value
+
+            if curr_sum == target:
+                left_index = paired[left][1]
+                right_index = paired[right][1]
+                result = [left_index , right_index ]
+                result.sort()
+                return result
+            elif curr_sum < target:
+                left += 1
+            else:
+                right -= 1
+        return []
