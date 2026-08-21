0ffb39abaed7b687a439ea8276cc4157738ad662
Ghaziah Shoeb <116344438+GhaziahShoeb@users.noreply.github.com>
2026-08-21 18:25:50 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (17.44%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,12 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        unique = []
-        for num in nums:
-            if len(unique) == 0:
-                unique.append(num)
-            elif num != unique[-1]:
-                unique.append(num) # since it is sorted so duplicates are aaju baju so the one going into the list and the last went is same if duplicate
-        for i in range(len(unique)):
-            nums[i] = unique[i]
-
-        return len(unique)
+        slow = 0
+        for fast in range(1, len(nums)):
+            if nums[fast] != nums[slow]:
+                slow += 1
+                nums[slow] = nums[fast]
+        return slow + 1
