5e61877c9bcca796ad681885e2f8748076009cc3
Ghaziah Shoeb <116344438+GhaziahShoeb@users.noreply.github.com>
2026-09-17 00:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 19.7 MB (14.21%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.py
@@ -0,0 +1,50 @@
+class Solution:
+    def searchMatrix(self, matrix: list[list[int]], target: int) -> bool:
+
+        row_idx = self.searchPotentialRow(matrix, target)
+
+        if row_idx != -1:
+            return self.binarySearchOverRow(row_idx, matrix, target)
+        else:
+            return False
+
+    def searchPotentialRow(self, matrix, target):
+
+        low = 0
+        high = len(matrix) - 1
+
+        while low <= high:
+
+            mid = low + (high - low) // 2
+
+            # Is target inside this row's range?
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                return mid
+
+            elif matrix[mid][0] < target:
+                low = mid + 1
+
+            else:
+                high = mid - 1
+
+        return -1
+
+    def binarySearchOverRow(self, row_idx, matrix, target):
+
+        low = 0
+        high = len(matrix[row_idx]) - 1
+
+        while low <= high:
+
+            mid = low + (high - low) // 2
+
+            if matrix[row_idx][mid] == target:
+                return True
+
+            elif matrix[row_idx][mid] > target:
+                high = mid - 1
+
+            else:
+                low = mid + 1
+
+        return False
